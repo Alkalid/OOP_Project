cb780e2088187701762dcaaa38ee3b98153c32c5
KaiRa <KaiRa@Ka1Ra>
2021-01-13 14:12:34 +0800

    type 3rd build

--- src/component/itemType.java
+++ src/component/itemType.java
@@ -3,5 +3,14 @@ package component;
 import java.util.List;
 
 public abstract class itemType {
-  List<String> type;
+  private List<String> type;
+  
+  public boolean isType(String compare) {
+    for(String find: type) {
+      if(compare.equals(find) == true) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
