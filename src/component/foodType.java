c452c8da592378b21f64f7941e340de272f73733
KaiRa <KaiRa@Ka1Ra>
2021-01-13 12:40:16 +0800

    type 2nd build

--- src/component/foodType.java
+++ src/component/foodType.java
@@ -5,4 +5,13 @@ import java.util.List;
 
 public class foodType extends itemType {
   List<String> type = new ArrayList<>();
+  
+  public foodType() {
+    type.add("Drinks");
+    type.add("Brunch");
+    type.add("Taiwanese food");
+    type.add("Ameracan food");
+    type.add("Japanese food");
+    type.add("Fast food");
+  }
 }
--- src/component/productType.java
+++ src/component/productType.java
@@ -5,4 +5,12 @@ import java.util.List;
 
 public class productType extends itemType {
   List<String> type = new ArrayList<>();
+  
+  public productType() {
+    type.add("Cloth");
+    type.add("Shoes");
+    type.add("3C");
+    type.add("Book");
+    type.add("Misc");
+  }
 }
