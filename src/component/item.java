bbc97b4465167ec3bf198cda1ea6462016bcd266
alkaloid <kartd80165@gmail.com>
2021-01-13 11:44:43 +0800

    新增 getter setter

--- src/component/item.java
+++ src/component/item.java
@@ -4,9 +4,24 @@ public abstract class item {
 	int price;
 	String item_id;
 	String name;
-	type type;
+	itemType type;
 	shop shop;
 	Seller seller;
 	
+	public item() {}
+	
+	public abstract void set();
+
+	public abstract itemType getType();
+
+	public abstract void setType(itemType type);
+	
+	public abstract shop getShop();
+
+	public abstract void setShop(shop shop);
+
+	public abstract Seller getSeller();
+
+	public abstract void setSeller(Seller seller);
 	
 }
