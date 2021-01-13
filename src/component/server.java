a636cd40272231e23fc76388af3f1f87df89be4e
alkaloid <kartd80165@gmail.com>
2021-01-13 19:37:51 +0800

    在server.java的建構子讀取creatItem

--- src/component/server.java
+++ src/component/server.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 public abstract class server {
 	String name = "";				//此server name
 	
-	CreatItem CI = new CreatItem(); //包含 目前CreatItem還沒做好
+	CreateItem CI = new CreateItem(); //包含 目前CreatItem還沒做好
 	
 	//購買資料 姓名 有哪些東西 外送方式 付款方式
 	
@@ -13,7 +13,11 @@ public abstract class server {
 	//item 陣列 表示此服務有多少項目
 	
 	public server() {
-		CI.getItem;	//取得CreatItem 製造的物品 放進LinkedList陣列中
+		item[] items= CI.getItemList();	//取得CreatItem 製造的物品 放進LinkedList陣列中
+		for(int i = 0; i < items.length; i++)
+		{
+			item_list.add(items[i]);
+		}
 	}
 	
 	public LinkedList getItemList() {
